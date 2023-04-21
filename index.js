716d653cc8756efc5d86471b70374925db6f2ae4
bubun123-git <bubundcool@gmail.com>
2023-04-21 12:09:15 +0530

    savind data in localstorage

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ function SaveToLocalStorage(event) {
     const name1 = event.target.username.value
     const email = event.target.emailId.value
     const phone = event.target.phonenumber.value
-    localStorage.setItem('name', name1)
-    localStorage.setItem('email', email)
-    localStorage.setItem('phone', phone)
+    // localStorage.setItem('name', name1)
+    //  localStorage.setItem('email', email)
+    //  localStorage.setItem('phone', phone)
 
     let myObj = {
         name1,
         email,
         phone,
     }
-    let objStringify = JSON.stringify(myObj)
-    console.log(objStringify)
+    localStorage.setItem(obj.email, JSON.stringify(myObj))
+    showUserOnScreen(myObj)
 
+
+}
+function showUserOnScreen() {
+    const ParentElement = document.getElementById('listofitems')
+    ParentElement.innerHTML = pare.innerHTML + $(obj.name) - $(obj.emailId) - $(obj.phonenumber)
 }
