3cea9ae77a81dd86a007a1e1f30faa27fe6a591a
bubun123-git <bubundcool@gmail.com>
2023-04-21 10:57:04 +0530

    stringify

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function SaveToLocalStorage(event) {
-    event.preventDefault();
+    event.preventDefault()
     const name1 = event.target.username.value
     const email = event.target.emailId.value
     const phone = event.target.phonenumber.value
     localStorage.setItem('name', name1)
     localStorage.setItem('email', email)
-    localStorage.setItem('phone', phonenumber)
+    localStorage.setItem('phone', phone)
+
+    let myObj = {
+        name1,
+        email,
+        phone,
+    }
+    let objStringify = JSON.stringify(myObj)
+    console.log(objStringify)
 
 }
